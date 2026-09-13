df20fec1860caa1a7bfc2db102a3fdc52e7af4c4
lucasgomes14 <lucazmatehus14@gmail.com>
2026-09-13 02:17:53 -0300

    feature - vercel configuração

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
+let app: any;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.init();
+  }
+  return app;
 }
-await bootstrap();
+
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap().then((app) => {
+    app.listen(3000, () => {
+      console.log('Servidor rodando localmente na porta 3000');
+    });
+  });
+}
+
+export default async function (req: any, res: any) {
+  const app = await bootstrap();
+  const instance = app.getHttpAdapter().getInstance();
+  return instance(req, res);
+}
