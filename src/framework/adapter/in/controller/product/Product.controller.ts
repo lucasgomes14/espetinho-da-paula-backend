5d2e66a91b23377f054dfd88dadc08478e0a1491
lucasgomes14 <lucazmatehus14@gmail.com>
2026-09-15 21:25:49 -0300

    feature - remoção de produto

--- src/framework/adapter/in/controller/product/Product.controller.ts
+++ src/framework/adapter/in/controller/product/Product.controller.ts
@@ -1,4 +1,16 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Inject, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { SaveProductDTO } from '../../dto/product/SaveProductDTO.js';
 import { PRODUCT_PORT_IN } from '../../../../../application/port/in/product/ProductPortIn.js';
 import type { ProductPortIn } from '../../../../../application/port/in/product/ProductPortIn.js';
@@ -34,5 +46,11 @@ export class ProductController {
   @HttpCode(HttpStatus.OK)
   async updateProduct(@Param("id", ParseIntPipe) id: number, @Body() dto: UpdateProductDTO ): Promise<void> {
     await this.productPortIn.updateProduct(id, dto);
+  }
+
+  @Delete(":id")
+  @HttpCode(HttpStatus.OK)
+  async deleteProduct(@Param("id", ParseIntPipe) id: number): Promise<void> {
+    await this.productPortIn.deleteProduct(id);
   }
 }
